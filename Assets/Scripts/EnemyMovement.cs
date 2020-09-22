96d28e6241d358cc6181355b4543589b4e16b487
anurajbh <anurajbh@users.noreply.github.com>
2020-09-22 12:00:47 -0400

    Moving enemy block

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> Path;
+    void Start()
+    {
+        StartCoroutine(MoveToWaypoints());
+    }
+
+    IEnumerator MoveToWaypoints()
+    {
+        print("Starting patrol");
+        foreach (Waypoint waypoint in Path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting waypoint : " + waypoint.gameObject.name);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
