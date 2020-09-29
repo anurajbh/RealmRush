5ae5901c8a4863231f75fc48103538148a939322
anurajbh <anurajbh@users.noreply.github.com>
2020-09-29 13:08:54 -0400

    Implemented the BFS algorithm

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    public Waypoint start;
+    public Waypoint end;
+    Vector2Int[] directions = { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left };
+    Queue<Waypoint> waypointsQueue = new Queue<Waypoint>();
+    [SerializeField] bool isEndFound = false;//todo make private
+    void LoadTheBlocks()
+    {
+        Waypoint[] waypoints = FindObjectsOfType<Waypoint>();
+        foreach(Waypoint waypoint in waypoints)
+        {
+            //overlapping blocks
+            bool isOverlapping = grid.ContainsKey(waypoint.GetGridPos());
+            //add to dictionary
+            if(!isOverlapping)
+            {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+                print("Adding Waypoint " + grid[waypoint.GetGridPos()] + " to Dictionary");//todo remove later
+            }
+
+        }
+    }
+    private void Start()
+    {
+        LoadTheBlocks();
+        ColourStartAndEnd();
+        Pathfind();
+    }
+
+    private void Pathfind()//implementation of the BFS algorithm
+    {
+        print("Enqueing " + grid[start.GetGridPos()]);
+        waypointsQueue.Enqueue(grid[start.GetGridPos()]);
+        while(waypointsQueue.Count!=0 && !isEndFound)
+        {
+
+            Waypoint currentWaypoint = waypointsQueue.Dequeue();
+            print("Dequeuing " + currentWaypoint);
+            currentWaypoint.isExplored = true;
+            isEndFound = HaltIfSearchFound(currentWaypoint);
+            ExploreNeighbours(currentWaypoint);
+
+        }
+    }
+
+    private bool HaltIfSearchFound(Waypoint currentWaypoint)
+    {
+        if (currentWaypoint == end)
+        {
+            print("Found the last waypoint");//todo remove later
+            return true;
+        }
+        return false;
+    }
+
+    private void ExploreNeighbours(Waypoint waypoint)
+    {
+        if(isEndFound)
+        {
+            return;
+        }
+        foreach(Vector2Int direction in directions)
+        {
+            Vector2Int neighbourCoordinates = waypoint.GetGridPos() + direction;
+            print("Exploring : Cube(" + neighbourCoordinates.x + ", " + neighbourCoordinates.y + ")");
+            try
+            {
+                QueueNewNeighbour(neighbourCoordinates);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    private void QueueNewNeighbour(Vector2Int neighbourCoordinates)
+    {
+        Waypoint neighbour = grid[neighbourCoordinates];
+        if(!neighbour.isExplored)
+        {
+            neighbour.SetTopColour(Color.blue);
+            print("Enqueing neighbour "+neighbour);
+            waypointsQueue.Enqueue(neighbour);
+        }
+
+    }
+
+    private void ColourStartAndEnd()
+    {
+        print("Colouring "+start);
+        start.SetTopColour(Color.green);
+        print("Colouring "+end);
+        end.SetTopColour(Color.black);
+    }
+}
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -7,15 +7,21 @@ public class Waypoint : MonoBehaviour
     // Start is called before the first frame update
     const int gridSize = 10;
     Vector2Int gridPos;
+    public bool isExplored = false;
     public int GetGridSize()
     {
         return gridSize;
     }
     // Update is called once per frame
-    public Vector2 GetGridPos()
+    public Vector2Int GetGridPos()
     {
-        return new Vector2(
-        Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
-        Mathf.RoundToInt(transform.position.z / gridSize) * gridSize);
+        return new Vector2Int(
+        Mathf.RoundToInt(transform.position.x / gridSize),
+        Mathf.RoundToInt(transform.position.z / gridSize));
+    }
+    public void SetTopColour(Color color)
+    {
+        MeshRenderer meshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
+        meshRenderer.material.color = color;
     }
 }
