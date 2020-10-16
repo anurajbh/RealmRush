541714196d75abfef072e3b6a4cff7cd9b34e0fa
anurajbh <anurajbh@users.noreply.github.com>
2020-10-16 16:50:21 -0400

    Tower, initial Hitpoints and shooting particle effects

---
+++ Assets/Scripts/Hitpoints.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitpoints : MonoBehaviour
+{
+    public float HP;
+    public void CheckForDeath()
+    {
+        if(HP <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void AffectThisObject(float affect)
+    {
+        HP += affect;
+    }
+    public void OnParticleCollision(GameObject other)
+    {
+        print("Lol Im hit");
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform panObject;
+    [SerializeField] Transform objectToLookAt;
+    public bool isFiring = false;
+    void Update()
+    {
+        LookAtEnemy(objectToLookAt);
+    }
+    public void LookAtEnemy(Transform objectToLook)
+    {
+        panObject.LookAt(objectToLook);
+    }
+}
