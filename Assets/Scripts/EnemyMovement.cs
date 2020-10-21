de7e7585d96e47cf9ad91e65bbea8e83969e7aa3
anurajbh <anurajbh@users.noreply.github.com>
2020-10-21 08:30:33 -0400

    Smooth Enemy Movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    List<Waypoint> enemyPath = new List<Waypoint>();
+    Transform movePoint;
+    [SerializeField] float moveSpeed = 10f;
     void Start()
     {
         Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
         List<Waypoint> enemyPath = pathfinder.GetPath();
+        transform.position = enemyPath[0].transform.position;
         StartCoroutine(MoveToWaypoints(enemyPath));
     }
-
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
     IEnumerator MoveToWaypoints(List<Waypoint> Path)
     {
         foreach (Waypoint waypoint in Path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(2f);
+            movePoint = waypoint.transform;
+            yield return new WaitUntil(() => isAtPosition(waypoint.transform) == true);
         }
+    }
+    bool isAtPosition(Transform movePoint)
+    {
+        if(transform.position==movePoint.position)
+        {
+            return true;
+        }
+        return false;
     }
 }
